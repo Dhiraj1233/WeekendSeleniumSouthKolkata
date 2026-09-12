eeb189a91507647de30311faf62c681dab839bb9
Dhiraj kumar <dhirajkryadav1999@gmail.com>
2026-09-12 11:53:53 +0530

    DataProvider

---
+++ src/test/java/com/qsp/utility/DataProviderUtility1.java
@@ -0,0 +1,13 @@
+package com.qsp.utility;
+
+import org.testng.annotations.DataProvider;
+
+public class DataProviderUtility1 {
+	
+	@DataProvider(name="InValidData")
+	public void loginInvalidData()
+	{
+		System.out.println("This is for git example");
+	}
+
+}
